7445e1f2a5058c0bbe3127c7b0ab14a8f9656f89
Sam Skeen <sskeen@sskeendevelopment.onmicrosoft.com>
2015-10-30 11:12:30 -0600

    Changes

---
+++ app/app.tsx
@@ -0,0 +1,26 @@
+// // let myFn = (name="Tom", age=21) => {
+// // 	console.log("best =", name)
+// // };
+
+// // myFn()
+
+// let { city, state, zip } = getAddress();
+
+// console.log(city, state, zip);
+
+
+// function getAddress() {
+// 	return {
+// 		name: 'Sam',
+// 		age: 23,
+// 		city: 'Lehi',
+// 		state: 'UT',
+// 		zip: '84101'
+// 	};
+// }
+
+let nums = [1, 2, 3];
+
+let [one,, three] = nums;
+
+console.log(one, three);
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,19 @@
 var path = require('path')
-var appPath = path.resolve(__dirname, 'src', 'index.js')
-var buildPath = path.resolve(__dirname, 'public')
+var appPath = path.resolve(__dirname, 'app', 'app.tsx')
+var buildPath = path.resolve(__dirname, 'public', 'js')
 
 module.exports = {
 
   entry: appPath,
+  
+  resolve: {
+    extensions: ['', 'webpack.js', '.js', '.ts', '.tsx']
+  },
+  
+  // devtools: '',
 
   output: {
-    filename: 'app.js',
-    path: buildPath,
-    publicPath: 'public'
+    filename: buildPath + '/bundle.js'
   },
 
   module: {
@@ -17,7 +21,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loaders: ['babel-loader']
+        loaders: ['ts-loader']
       }
     ]
   }
